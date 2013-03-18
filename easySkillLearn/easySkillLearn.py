9aebbccb71645c7b3e0d777c5b155afc3adb56dd
wss445566 <wss445566@gmail.com>
2013-03-18 15:10:27 +0800

    修正及優化 easySkillLearn
    修正 單一技能學習錯誤 產生無限迴圈問題.
    縮短 "全部學習" 遇到有問題技能時的回應時間.

--- easySkillLearn/easySkillLearn.py
+++ easySkillLearn/easySkillLearn.py
@@ -1,6 +1,3 @@
-import sys
-from com.l2jserver.gameserver.model.quest		import State
-from com.l2jserver.gameserver.model.quest		import QuestState
 from com.l2jserver.gameserver.model.quest.jython	import QuestJython as JQuest
 
 from com.l2jserver.gameserver.datatables import SkillTable #技能中文名相關
@@ -38,31 +35,43 @@ class EasySkillLearn(JQuest):
 		sl = self.getAvailableSkill(player)
 		r = "可學習的技能清單<br1>"
 		r += '<a action="bypass -h Quest %s learn_all">全部學習</a><br1>' % self.qn
-		skillTable = SkillTable.getInstance()
 		for s in sl:
-			r += "<a action=\"bypass -h Quest " + self.qn + " learn " + str(s.getSkillId()) + " " + str(s.getSkillLevel()) + "\">" + skillTable.getInfo(s.getSkillId(), s.getSkillLevel()).getName() + " Lv " + str(s.getSkillLevel()) + "</a><BR1>"
+			if s and s.getName():
+				r += "<a action=\"bypass -h Quest " + self.qn + " learn " + str(s.getSkillId()) + " " + str(s.getSkillLevel()) + "\">" + s.getName() + " Lv " + str(s.getSkillLevel()) + "</a><BR1>"
 		return r
 
 	def myAddSkill(self, player, id):
+		oldSkill = player.getKnownSkill(id)
 		sl = self.getAvailableSkill(player)
 		for s in sl:
 			if s.getSkillId() == id:
-				player.addSkill(SkillTable.getInstance().getInfo(s.getSkillId(), s.getSkillLevel()), True)
+				newskill = SkillTable.getInstance().getInfo(s.getSkillId(), s.getSkillLevel())
+				if newskill:
+					player.addSkill(newskill, True)
+				else:
+					return 1
+				if oldSkill == player.getKnownSkill(id):
+					print self.qn, player, "學習技能錯誤", id, s.getName()
+					player.sendMessage("學習技能錯誤 %s" % s.getName())
+					return 1
 				self.myAddSkill(player, id)
 				break
+		return 0
 
 	def onAdvEvent(self, event, npc, player):
 		if event == 'learn_all':
 			sl = self.getAvailableSkill(player)
-			c = 0
-			while sl:
-				player.addSkill(SkillTable.getInstance().getInfo(sl[0].getSkillId(), sl[0].getSkillLevel()), True)
-				sl = self.getAvailableSkill(player)
-				c += 1
-				if c > 5000:
-					return self.htm_header + "未能全部技能學完 請再試一次 或與 GM 聯絡" + self.htm_footer #技能資料出現問題 請回報
+			for s in sl:
+				self.myAddSkill(player, s.getSkillId())
+			#c = 0
+			#while sl:
+			#	player.addSkill(SkillTable.getInstance().getInfo(sl[0].getSkillId(), sl[0].getSkillLevel()), True)
+			#	sl = self.getAvailableSkill(player)
+			#	c += 1
+			#	if c > 5000:
+			#		return self.htm_header + "未能全部技能學完 請再試一次 或與 GM 聯絡" + self.htm_footer #技能資料出現問題 請回報
 			player.sendSkillList()
-			return self.htm_header + "全部技能學完 %s" % ["(不包括遺忘秘傳技能)","(包括遺忘秘傳技能)"][self.includeByFs] + self.htm_footer
+			return self.htm_header + "學習過程完成. %s" % ["(不包括遺忘秘傳技能)","(包括遺忘秘傳技能)"][self.includeByFs] + self.htm_footer
 		if event.startswith('learn '):
 			sid, slv = event[6:].split()
 			sid = int(sid)
@@ -72,10 +81,6 @@ class EasySkillLearn(JQuest):
 		return self.onFirstTalk(npc, player)
 		
 	def onFirstTalk(self, npc, player):
-		st = player.getQuestState(self.qn)
-		if not st:
-			st = self.newQuestState(player)
-			st.setState(State.STARTED)
 		return self.htm_header + self.listAvailableSkill(player) + self.htm_footer
 		
 EasySkillLearn()
