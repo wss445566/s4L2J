415a53a1cd3367f83b42eb4d845937ea13995a0f
wss445566 <wss445566@gmail.com>
2013-04-01 18:01:46 +0800

    MyAI.py 加入 歐塔比斯.

--- MyAI/MyAI.py
+++ MyAI/MyAI.py
@@ -6,35 +6,61 @@ from com.l2jserver.gameserver.model import L2CharPosition
 npc29191 = 29191 #歐塔比斯
 npc29192 = 29192 #歐塔比斯野獸
 
-class MyAI(L2CharacterAI):
-	runningCircleIndex = 0
-	runningCircleSteps = 36
-	radius = 1000
-	circlePoint = None
+class MyAI29191(L2CharacterAI):#歐塔比斯
 	def __init__(self, accessor):
 		L2CharacterAI.__init__(self, accessor)
-		npc = self.getActor()
-		if npc.getNpcId() == npc29192:
-			s = npc.getSpawn()
-			self.circlePoint = self.plotCircle(s.getLocx(), s.getLocy(), self.radius, self.runningCircleSteps)
-			self.onEvtThink()
+		
+	def onEvtThink(self):
+		pass
 		
 	def onEvtAttacked(self, attacker):
 		pass
 		
+
+class MyAI29192(L2CharacterAI):#歐塔比斯野獸
+	runningCircleIndex = 0
+	runningCircleSteps = 36
+	radius = 1000
+	circlePoint = None
+	npc = None
+	def __init__(self, accessor):
+		L2CharacterAI.__init__(self, accessor)
+		self.npc = self.getActor()
+		if self.npc.getNpcId() == npc29192:
+			s = self.npc.getSpawn()
+			self.circlePoint = self.plotCircle(s.getLocx(), s.getLocy(), self.radius, self.runningCircleSteps)
+			self.onEvtThink()
+		
 	def onEvtArrived(self):
+		pass
+	def onEvtCancel(self):
+		pass
+	def onEvtConfused(self, attacker):
+		pass
+	def onEvtFakeDeath(self):
+		pass
+	def onEvtForgetObject(self, object):
+		pass
+	def onEvtParalyzed(self, attacker):
+		pass
+	def onEvtRooted(self, attacker):
+		pass	
+	def onEvtSleeping(self, attacker):
+		pass
+	def onEvtStunned(self, attacker):
+		pass
+	def onEvtAttacked(self, attacker):
 		pass
 		
 	def onEvtArrived(self):
-		npc = self.getActor()
-		if npc.getNpcId() == npc29192:
+		if self.npc.getNpcId() == npc29192:
 			x, y = self.circlePoint[self.runningCircleIndex]
 			self.runningCircleIndex += 1
 			if 0 < self.runningCircleIndex >= self.runningCircleSteps:
 				self.runningCircleIndex = 0
-			s = npc.getSpawn()
+			s = self.npc.getSpawn()
 			self.setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, L2CharPosition(x, y, s.getLocz(), s.getHeading()))
-			npc.setRunning()
+			self.npc.setRunning()
 		
 	def onEvtThink(self):
 		self.onEvtArrived()
@@ -67,9 +93,35 @@ class Quest(QuestJython):
 
 	def onSpawn(self, npc):
 		if npc.getNpcId() == npc29191:
-			pass
+			npc.setAI(MyAI29191(AIAccessor(npc)))
+			
 		if npc.getNpcId() == npc29192:
-			npc.setAI(MyAI(AIAccessor(npc)))
+			npc.setAI(MyAI29192(AIAccessor(npc)))
+			s = npc.getSpawn()
+			instid = s.getInstanceId()
+			nextNpc = self.spawnNpc(npc29191, s.getLocx(), s.getLocy(), s.getLocz(), s.getHeading(), instid)
+			if nextNpc:
+				nextNpc.setRunning()
+				nextNpc.setIsInvul(True)
+				nextNpc.getAI().startFollow(npc)
+
+			
+	def spawnNpc(self, npcId, x, y, z, heading, instid):
+		from com.l2jserver.gameserver.datatables import NpcTable
+		from com.l2jserver.gameserver.model import L2Spawn
+		from com.l2jserver.gameserver.datatables import SpawnTable
+		npcTemplate = NpcTable.getInstance().getTemplate(npcId)
+		npcSpawn = L2Spawn(npcTemplate)
+		npcSpawn.setLocx(x)
+		npcSpawn.setLocy(y)
+		npcSpawn.setLocz(z)
+		npcSpawn.setHeading(heading)
+		npcSpawn.setAmount(1)
+		npcSpawn.setInstanceId(instid)
+		SpawnTable.getInstance().addNewSpawn(npcSpawn, False)
+		npc = npcSpawn.doSpawn()
+		#npc.setOnKillDelay(0)
+		return npc
 		
 		
 Quest()
