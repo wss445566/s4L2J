40c35917312b0fc6ef1835ac9a6fce6221525a37
wss445566 <wss445566@gmail.com>
2013-04-12 03:19:42 +0800

    vcPlayerAnno 增加道具需要.

--- vcPlayerAnno/vcPlayerAnno.py
+++ vcPlayerAnno/vcPlayerAnno.py
@@ -1,15 +1,32 @@
 from com.l2jserver.gameserver.handler import VoicedCommandHandler
 from com.l2jserver.gameserver.handler import IVoicedCommandHandler
-
+from com.l2jserver.gameserver.datatables import ItemTable
+from com.l2jserver.gameserver.network.serverpackets import InventoryUpdate
 from com.l2jserver.gameserver import Announcements
 
 class VCPlayerAnno(IVoicedCommandHandler):
 	isCritical = True #是否為重要公告形式顯示
+	requireItemId = 57
+	requireItemCount = 100
 
 	commands = ["公告"]
 	
 	def useVoicedCommand(self, command, player, params):
-		Announcements.getInstance().announceToAll("%s:%s" % (player.getName(), params), self.isCritical)
+		if not player: return
+		inv = player.getInventory()
+		if not inv: return
+		ditem = inv.destroyItemByItemId("vcplayeranno", self.requireItemId, self.requireItemCount, player, None) 
+		if ditem:
+			Announcements.getInstance().announceToAll("%s:%s" % (player.getName(), params), self.isCritical)
+			iu = InventoryUpdate()
+			iu.addModifiedItem(ditem)
+			player.sendPacket(iu)
+		else:
+			item = ItemTable.getInstance().getTemplate(self.requireItemId)
+			name = ""
+			if item:
+				name = item.getName()
+			player.sendMessage("所需道具不足 需要 %s %d 個" % (name, self.requireItemCount))
 			
 	def getVoicedCommandList(self):
 		return self.commands
