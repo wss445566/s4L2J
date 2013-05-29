a0814de13e7c80138916651ff3725eebe143419e
wss445566 <wss445566@gmail.com>
2013-05-29 14:12:31 +0800

    gen_item_xml 修正檔案內起始 ID

--- XmlGen/gen_item_xml.py
+++ XmlGen/gen_item_xml.py
@@ -178,190 +178,189 @@ def addSetNode(p, **kw):
 def process_itemname(itemname, root):
     for key in itemname.f:
         if not key in root:
-            root[key] = ET.Element('item', attrib={'id':itemname.f[key][0],'name':itemname.f[key][1]})
-        else:
-            item = root[key]
-            f = itemname.f[key]
-            item.set('id', f[0])
-            item.set('name', f[1])
+            root[key] = ET.Element('item', attrib={'id':str(key),'name':itemname.f[key][1]})
+        item = root[key]
+        f = itemname.f[key]
+        #item.set('id', f[0])
+        item.set('name', f[1])
             
 def process_weapongrp(weapongrp, root):
     for key in weapongrp.f:
         if not key in root:
-            root[key] = ET.Element('item')
-        else:
-            item = root[key]
-            f = weapongrp.f[key]
-            item.set('type', u'Weapon')
-            addSetNode(
-                item
-                , icon=f[22]
-                , weight=f[28]
-                , default_action='equip'
-            )
-            if int(f[27]) > 0:
-                addSetNode(item, duration=f[27])
-            if f[29] in material:
-                addSetNode(item, material=material[f[29]])
-            if not f[30] == '0':
-                addSetNode(item, enchant_enabled=f[30])
-            if not f[32] == '0':
-                addSetNode(item
-                    , is_tradable="false"
-                    , is_dropable="false"
-                    , is_sellable="false"
-                    , is_depositable="false"
-                    , is_questitem="true")
-            if f[38] in bodypart:
-                addSetNode(item, bodypart=bodypart[f[38]])
-            if not f[58] == '0':
-                addSetNode(item, random_damage=f[58])
-            if f[59] in weapon_type:
-                addSetNode(item, weapon_type=weapon_type[f[59]])
-            if f[60] in crystal_type:
-                if not f[60] == '0':
-                    addSetNode(item, crystal_type=crystal_type[f[60]], crystal_count="1")
-            if int(f[61]) > 0:
-                addSetNode(item, mp_consume=f[61])
-            if int(f[62]) > 0:
-                addSetNode(item, soulshots=f[62])
-            if int(f[63]) > 0:
-                addSetNode(item, spiritshots=f[63])
-            if int(f[66]) > 0:
-                setE = ET.SubElement(item, 'cond', attrib={'msgId':'1518'})
-                ET.SubElement(setE, 'player', attrib={'isHero':'true'})
+            root[key] = ET.Element('item', attrib={'id':str(key),'name':'unknow'})
+            print "warning weapongrp ", key
+        item = root[key]
+        f = weapongrp.f[key]
+        item.set('type', u'Weapon')
+        addSetNode(
+            item
+            , icon=f[22]
+            , weight=f[28]
+            , default_action='equip'
+        )
+        if int(f[27]) > 0:
+            addSetNode(item, duration=f[27])
+        if f[29] in material:
+            addSetNode(item, material=material[f[29]])
+        if not f[30] == '0':
+            addSetNode(item, enchant_enabled=f[30])
+        if not f[32] == '0':
+            addSetNode(item
+                , is_tradable="false"
+                , is_dropable="false"
+                , is_sellable="false"
+                , is_depositable="false"
+                , is_questitem="true")
+        if f[38] in bodypart:
+            addSetNode(item, bodypart=bodypart[f[38]])
+        if not f[58] == '0':
+            addSetNode(item, random_damage=f[58])
+        if f[59] in weapon_type:
+            addSetNode(item, weapon_type=weapon_type[f[59]])
+        if f[60] in crystal_type:
+            if not f[60] == '0':
+                addSetNode(item, crystal_type=crystal_type[f[60]], crystal_count="1")
+        if int(f[61]) > 0:
+            addSetNode(item, mp_consume=f[61])
+        if int(f[62]) > 0:
+            addSetNode(item, soulshots=f[62])
+        if int(f[63]) > 0:
+            addSetNode(item, spiritshots=f[63])
+        if int(f[66]) > 0:
+            setE = ET.SubElement(item, 'cond', attrib={'msgId':'1518'})
+            ET.SubElement(setE, 'player', attrib={'isHero':'true'})
                 
 def process_armorgrp(armorgrp, root):
     for key in armorgrp.f:
         if not key in root:
-            root[key] = ET.Element('item')
+            root[key] = ET.Element('item', attrib={'id':str(key),'name':'unknow'})
+            print "warning armorgrp ", key
+        item = root[key]
+        f = armorgrp.f[key]
+        item.set('type', u'Armor')
+        addSetNode(
+            item
+            , icon=f[22]
+            , weight=f[28]
+            , default_action='equip'
+        )
+        if int(f[27]) > 0:
+            addSetNode(item, duration=f[27])
+        if f[29] in material:
+            addSetNode(item, material=material[f[29]])
+        if not f[30] == '0':
+            addSetNode(item, enchant_enabled=f[30])
+        if not f[32] == '0':
+            addSetNode(item
+                , is_tradable="false"
+                , is_dropable="false"
+                , is_sellable="false"
+                , is_depositable="false"
+                , is_questitem="true")
+        if f[38] in bodypart:
+            addSetNode(item, bodypart=bodypart[f[38]])
+        if f[233] in armor_type:
+            addSetNode(item, armor_type=armor_type[f[233]])
+        if f[234] in crystal_type:
+            if not f[234] == '0':
+                addSetNode(item, crystal_type=crystal_type[f[234]], crystal_count="1")
         else:
-            item = root[key]
-            f = armorgrp.f[key]
-            item.set('type', u'Armor')
-            addSetNode(
-                item
-                , icon=f[22]
-                , weight=f[28]
-                , default_action='equip'
-            )
-            if int(f[27]) > 0:
-                addSetNode(item, duration=f[27])
-            if f[29] in material:
-                addSetNode(item, material=material[f[29]])
-            if not f[30] == '0':
-                addSetNode(item, enchant_enabled=f[30])
-            if not f[32] == '0':
-                addSetNode(item
-                    , is_tradable="false"
-                    , is_dropable="false"
-                    , is_sellable="false"
-                    , is_depositable="false"
-                    , is_questitem="true")
-            if f[38] in bodypart:
-                addSetNode(item, bodypart=bodypart[f[38]])
-            if f[233] in armor_type:
-                addSetNode(item, armor_type=armor_type[f[233]])
-            if f[234] in crystal_type:
-                if not f[234] == '0':
-                    addSetNode(item, crystal_type=crystal_type[f[234]], crystal_count="1")
-            else:
-                print key, f[234]
-            if int(f[235]) > 0:
-                fornode = item.find('for')
-                if fornode is None:
-                    fornode = ET.SubElement(item, 'for')
-                ET.SubElement(fornode, 'add', attrib={'order':'0x40','stat':'maxMp','val':f[235]})
-            #if not f[235] == '0':
-            #    print key
-                #for x in xrange(len(f)):
-                #    print x, f[x], armorgrp.title[x]
+            print key, f[234]
+        if int(f[235]) > 0:
+            fornode = item.find('for')
+            if fornode is None:
+                fornode = ET.SubElement(item, 'for')
+            ET.SubElement(fornode, 'add', attrib={'order':'0x40','stat':'maxMp','val':f[235]})
+        #if not f[235] == '0':
+        #    print key
+            #for x in xrange(len(f)):
+            #    print x, f[x], armorgrp.title[x]
 
 def process_etcitemgrp(etcitemgrp, root):
     for key in etcitemgrp.f:
         if not key in root:
-            root[key] = ET.Element('item')
-        else:
-            item = root[key]
-            f = etcitemgrp.f[key]
-            item.set('type', u'EtcItem')
-            addSetNode(
-                item
-                , icon=f[22]
-                #, default_action='equip'
-            )
-            if int(f[27]) > 0:
-                addSetNode(item, duration=f[27])
-            if int(f[28]) > 0:
-                addSetNode(item, weight=f[28])
-            if f[29] in material:
-                addSetNode(item, material=material[f[29]])
-            if not f[30] == '0':
-                addSetNode(item, enchant_enabled=f[30])
-            if not f[32] == '0':
-                addSetNode(item
-                    , is_tradable="false"
-                    , is_dropable="false"
-                    , is_sellable="false"
-                    , is_depositable="false"
-                    , is_questitem="true")
-            if not f[53] == '0':
-                addSetNode(item, is_stackable='true')
-            if f[54] in etcitem_type:
-                addSetNode(item, etcitem_type=etcitem_type[f[54]])
-            if f[55] in crystal_type:
-                if not f[55] == '0':
-                    addSetNode(item, crystal_type=crystal_type[f[55]], crystal_count="1")
+            root[key] = ET.Element('item', attrib={'id':str(key),'name':'unknow'})
+            print "warning etcitemgrp ", key
+        item = root[key]
+        f = etcitemgrp.f[key]
+        item.set('type', u'EtcItem')
+        addSetNode(
+            item
+            , icon=f[22]
+            #, default_action='equip'
+        )
+        if int(f[27]) > 0:
+            addSetNode(item, duration=f[27])
+        if int(f[28]) > 0:
+            addSetNode(item, weight=f[28])
+        if f[29] in material:
+            addSetNode(item, material=material[f[29]])
+        if not f[30] == '0':
+            addSetNode(item, enchant_enabled=f[30])
+        if not f[32] == '0':
+            addSetNode(item
+                , is_tradable="false"
+                , is_dropable="false"
+                , is_sellable="false"
+                , is_depositable="false"
+                , is_questitem="true")
+        if not f[53] == '0':
+            addSetNode(item, is_stackable='true')
+        if f[54] in etcitem_type:
+            addSetNode(item, etcitem_type=etcitem_type[f[54]])
+        if f[55] in crystal_type:
+            if not f[55] == '0':
+                addSetNode(item, crystal_type=crystal_type[f[55]], crystal_count="1")
 
 def process_itemstatdata(itemstatdata, root):
     for key in itemstatdata.f:
         if not key in root:
-            root[key] = ET.Element('item')
-        else:
-            item = root[key]
-            f = itemstatdata.f[key]
-            enchant_enable = not item.find("set[@name='enchant_enabled']") is None
-            fornode = item.find('for')
-            if fornode is None:
-                fornode = ET.SubElement(item, 'for')
-            if not float(f[1]) == 0:
-                ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'pDef','val':"%0.0f" % float(f[1])})
-                if enchant_enable:
-                    ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'pDef','val':'0'})
-            if not float(f[2]) == 0:
-                ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'mDef','val':"%0.0f" % float(f[2])})
-                if enchant_enable:
-                    ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'mDef','val':'0'})
-            if not float(f[3]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'pAtk','val':"%0.0f" % float(f[3])})
-                if enchant_enable:
-                    ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'pAtk','val':'0'})
-            if not float(f[4]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'mAtk','val':"%0.0f" % float(f[4])})
-                if enchant_enable:
-                    ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'mAtk','val':'0'})
-            if not float(f[6]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'pAtkSpd','val':"%0.0f" % float(f[6])})
-            if not float(f[7]) == 0:
-                if float(f[7]) > 0:
-                    ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'accCombat','val':"%0.2f" % float(f[7])})
-                else:
-                    ET.SubElement(fornode, 'sub', attrib={'order':'0x10','stat':'accCombat','val':"%0.2f" % abs(float(f[7]))})
-            if not float(f[9]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'rCrit','val':"%0.0f" % float(f[9])})
-            if not float(f[12]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'sDef','val':"%0.0f" % float(f[12])})
-                if enchant_enable:
-                    ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'sDef','val':'0'})
-            if not float(f[13]) == 0:
-                ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'rShld','val':"%0.0f" % float(f[13])})
-            if not float(f[14]) == 0:
-                if float(f[14]) > 0:
-                    ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'rEvas','val':"%0.0f" % float(f[14])})
-                else:
-                    ET.SubElement(fornode, 'sub', attrib={'order':'0x10','stat':'rEvas','val':"%0.0f" % abs(float(f[14]))})
-            if len(fornode) == 0:
-                item.remove(fornode)
+            root[key] = ET.Element('item', attrib={'id':str(key),'name':'unknow'})
+            print "warning itemstatdata ", key
+        item = root[key]
+        f = itemstatdata.f[key]
+        enchant_enable = not item.find("set[@name='enchant_enabled']") is None
+        fornode = item.find('for')
+        if fornode is None:
+            fornode = ET.SubElement(item, 'for')
+        if not float(f[1]) == 0:
+            ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'pDef','val':"%0.0f" % float(f[1])})
+            if enchant_enable:
+                ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'pDef','val':'0'})
+        if not float(f[2]) == 0:
+            ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'mDef','val':"%0.0f" % float(f[2])})
+            if enchant_enable:
+                ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'mDef','val':'0'})
+        if not float(f[3]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'pAtk','val':"%0.0f" % float(f[3])})
+            if enchant_enable:
+                ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'pAtk','val':'0'})
+        if not float(f[4]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'mAtk','val':"%0.0f" % float(f[4])})
+            if enchant_enable:
+                ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'mAtk','val':'0'})
+        if not float(f[6]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'pAtkSpd','val':"%0.0f" % float(f[6])})
+        if not float(f[7]) == 0:
+            if float(f[7]) > 0:
+                ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'accCombat','val':"%0.2f" % float(f[7])})
+            else:
+                ET.SubElement(fornode, 'sub', attrib={'order':'0x10','stat':'accCombat','val':"%0.2f" % abs(float(f[7]))})
+        if not float(f[9]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'rCrit','val':"%0.0f" % float(f[9])})
+        if not float(f[12]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'sDef','val':"%0.0f" % float(f[12])})
+            if enchant_enable:
+                ET.SubElement(fornode, 'enchant', attrib={'order':'0x0C','stat':'sDef','val':'0'})
+        if not float(f[13]) == 0:
+            ET.SubElement(fornode, 'set', attrib={'order':'0x08','stat':'rShld','val':"%0.0f" % float(f[13])})
+        if not float(f[14]) == 0:
+            if float(f[14]) > 0:
+                ET.SubElement(fornode, 'add', attrib={'order':'0x10','stat':'rEvas','val':"%0.0f" % float(f[14])})
+            else:
+                ET.SubElement(fornode, 'sub', attrib={'order':'0x10','stat':'rEvas','val':"%0.0f" % abs(float(f[14]))})
+        if len(fornode) == 0:
+            item.remove(fornode)
             
 def writeXML(startid, list_element):
     startid = "000%d00" % (int(startid)/100)
@@ -382,7 +381,7 @@ rstart = 0
 for key in sorted(root.keys()):
     if key > rstart + 99:
         writeXML(rstart, list_element)
-        rstart = key
+        rstart = key / 100 * 100
         list_element = ET.Element('list', attrib={"xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance", "xsi:noNamespaceSchemaLocation":"../../../xsd/items.xsd"})
     if len(root[key]) > 0:
         list_element.append(root[key])
